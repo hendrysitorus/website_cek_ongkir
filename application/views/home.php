5e08915854266c0d4f733c7b6f675f315d69abe9
hendry sitorus <hendrysitorus20@gmail.com>
2023-08-03 12:59:37 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cek Ongkos Kirim</title>
+</head>
+<body>
+    <h2>Mencek Ongkos Kirim</h2>
+</body>
+</html>
