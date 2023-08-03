25d97bd2b89f7c341f92fc6aa7ee0a1bd7587829
hendry sitorus <hendrysitorus20@gmail.com>
2023-08-03 13:32:12 +0700

    adding form cek ongkir

--- application/views/home.php
+++ application/views/home.php
@@ -4,8 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cek Ongkos Kirim</title>
+    <link href= " <?php echo base_url('asset/css/bootstrap.css')?>" rel = "stylesheet">
 </head>
 <body>
+    <div class="container">
     <h2>Mencek Ongkos Kirim</h2>
+     
+    <form>
+
+        <div class="form-group">
+        <label">Pilih Asal Kota </label>
+            <select class="form-control">
+                <option>Pilih Kota</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+        <label">Pilih Tujuan Kota </label>
+            <select class="form-control">
+                <option>Pilih Kota</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+        <label">Berat Paket</label>
+        <input type="text" name="berat" class="form-control">
+        </div>
+
+        <div class="form-group">
+        <label">Pilih Jenis Kurir </label>
+            <select class="form-control">
+                <option value="">Pilih Kurir</option>
+                <option value="jne">JNE</option>
+                <option value="tiki">TIKI</option>
+                <option value="pos">POS Indonesia</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Cek Ongkir" class="btn btn-primary" name="submit">
+        </div>
+
+    </form>
+
+    </div>
+    
 </body>
 </html>
